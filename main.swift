9d666d4da69fbba3c8b8595cb8255a2e062a28bf
Parinya Termkasipanich <40797988+parinyapt@users.noreply.github.com>
2022-02-12 17:37:13 +0000

    update finish sol.1

--- main.swift
+++ main.swift
@@ -5,40 +5,93 @@ import Foundation
 
 //ค่าน้ำประปา การคิดแบบอัตราก้าวหน้า
 //ยูนิตการใช้   ราคา  จำนวนหน่วย พิกัดสูงสุด(ราคา)
-//0-10      10.20   10        102           10
-//11-20     16.00   10        160           20
-//21-30     19.00   10        190           30
-//31-50     21.20   20        424           50
-//51-80     21.60   30        648           80
-//81-100    21.65   20        433           100
-//101-300   21.70   200       4340          300
-//301-1000  21.75   700       15225           1000
-//1001-2000 21.80   1000      21800           2000
-//2001-3000 21.85   1000      21850           3000
+//0-10      10.20   10        102           10    
+//11-20     16.00   10        160           20    262
+//21-30     19.00   10        190           30    452
+//31-50     21.20   20        424           50    876
+//51-80     21.60   30        648           80    1524
+//81-100    21.65   20        433           100   1957
+//101-300   21.70   200       4340          300   6297
+//301-1000  21.75   700       15225           1000    21522
+//1001-2000 21.80   1000      21800           2000    43322
+//2001-3000 21.85   1000      21850           3000    65172
 //>3000     21.90
 
-//Input จำนวนยูนิตการใช้น้ำ
-//0utput ราคาสุทธิ /vat 7% /ราคาก่อน vat7%
+//Input type / unit
+//0utput subtotal / vat 7% / total
 
-let unit0_10:Float = 10.20
-let unit11_20:Float = 16.00
-let unit21_30:Float = 19.00
-let unit31_50:Float = 21.20
-let unit51_80:Float = 21.60
-let unit81_100:Float = 21.65
-let unit101_300:Float = 21.70
-let unit301_1000:Float = 21.75
-let unit1001_2000:Float = 21.80
-let unit2001_3000:Float = 21.85
-let unit3000UP:Float = 21.90
+var unit0_10:Float = 0
+var unit11_20:Float = 0
+var unit21_30:Float = 0
+var unit31_50:Float = 0
+var unit51_80:Float = 0
+var unit81_100:Float = 0
+var unit101_300:Float = 0
+var unit301_1000:Float = 0
+var unit1001_2000:Float = 0
+var unit2001_3000:Float = 0
+var unit3000UP:Float = 0
 
-//input 3 1)meter no 2)start unit 3)end unit
-//output 3 1)subtotal 2)vat 3)total
+var type:Int = 0
 var unit:Int = 0
-var price:Float = 0.0
 
-print("ป้อนค่าจำนวนยูนิตการใช้น้ำ : ", terminator: "")
-unit = Int(readLine()!)!
+var price:Float = 0.0
+var vat:Float = 0.0
+var total:Float = 0.0
+
+//input Type
+repeat {
+  print("""
+  Type 1 : ที่อยู่อาศัย
+  Type 2 : ราชการและธุรกิจขนาดเล็ก
+  Type 3 : รัฐวิสาหกิจ/อุตฯ/ธุรกิจขนาดใหญ่
+  Enter Type (1 or 2 or 3) : 
+  """, terminator: "")
+  type = Int(readLine()!)!
+} while type != 1 && type != 2 && type != 3
+
+repeat {
+  print("Enter Volume (cu.m.) : ", terminator: "")
+  unit = Int(readLine()!)!
+} while unit < 0
+
+if type == 1 {
+  unit0_10 = 10.20
+  unit11_20 = 16.00
+  unit21_30 = 19.00
+  unit31_50 = 21.20
+  unit51_80 = 21.60
+  unit81_100 = 21.65
+  unit101_300 = 21.70
+  unit301_1000 = 21.75
+  unit1001_2000 = 21.80
+  unit2001_3000 = 21.85
+  unit3000UP = 21.90
+}else if type == 2 {
+  unit0_10 = 17.00
+  unit11_20 = 20.00
+  unit21_30 = 21.00
+  unit31_50 = 22.00
+  unit51_80 = 23.00
+  unit81_100 = 24.00
+  unit101_300 = 27.40
+  unit301_1000 = 27.50
+  unit1001_2000 = 27.60
+  unit2001_3000 = 27.80
+  unit3000UP = 28.00
+}else{
+  unit0_10 = 18.25
+  unit11_20 = 21.50
+  unit21_30 = 25.50
+  unit31_50 = 28.50
+  unit51_80 = 31.00
+  unit81_100 = 31.25
+  unit101_300 = 31.50
+  unit301_1000 = 31.75
+  unit1001_2000 = 32.00
+  unit2001_3000 = 32.25
+  unit3000UP = 32.50
+}
 
 if unit > 0 && unit < 11 {
   price = Float(unit) * unit0_10
@@ -84,5 +137,19 @@ else{
   price = price + (unit0_10 * 10) + (unit11_20 * 10) + (unit21_30 * 10) + (unit31_50 * 20) + (unit51_80 * 30) + (unit81_100 * 20) + (unit101_300 * 200) + (unit301_1000 * 700) + (unit1001_2000 * 1000) + (unit2001_3000 * 1000)
 }
 
-print("unit : \(unit)")
-print("Price : \(price)")
+//Calculate
+price = type == 2 && price < 150 ? 150 : price
+price = type == 3 && price < 300 ? 300 : price
+vat = price * 0.07
+total = price + vat
+
+//output
+print("""
+-------------WATER TARIFFS-------------
+Type : \(type)
+Volume : \(unit) cu.m.
+---------------------------------------
+Subtotal (Vat not included) : \(price) Baht
+Vat 7% : \(vat) Baht
+Total (Vat included) : \(total) Baht
+""")
